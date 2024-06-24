5b3cf8d9f67ef7393c023c4407d3d472295e9068
Iván Herrera <119714354+ivancho1212@users.noreply.github.com>
2024-06-24 11:56:20 -0500

    change routes

--- src/routes/personStatus.routes.js
+++ src/routes/personStatus.routes.js
@@ -1,11 +1,11 @@
 // personStatus.router.js
 import { Router } from 'express';
-import { showPersonStatus, createPersonStatus, updatePersonStatus, deletePersonStatus } from '../controllers/personStatus.controller.js';
+import { showPersonStatus, createPersonStatus, updatePersonStatus, showIdPersonStatus, deletePersonStatus } from '../controllers/personStatus.controller.js';
 
 const router = Router();
 
 router.get('/personStatus', showPersonStatus);
-router.get('/personStatus/:id', showPersonStatus);
+router.get('/personStatus/:id', showIdPersonStatus);
 router.post('/personStatus', createPersonStatus);
 router.put('/personStatus/:id', updatePersonStatus);
 router.delete('/personStatus/:id', deletePersonStatus);
